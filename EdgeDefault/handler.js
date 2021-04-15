ecaf39f647a4b190dadba2da64c531a1c17a68df
Paul Taylor <me@ptylr.com>
2021-04-15 23:49:22 +0100

    Refactoring to remove redundant Lambda Functions.

--- EdgeDefault/handler.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports.handler = (event, context, callback) => {
-    const request = event.Records[0].cf.request;
-    const olduri = request.uri;
-    request.uri = olduri.replace(/\/$/, '\/index.html');
-    console.log('Request for [' + olduri + '], rewritten to [' + request.uri + ']');
-    callback(null, request);
-};
